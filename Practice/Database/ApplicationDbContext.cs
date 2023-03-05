db888a892abc8c349b1d618938c1d5bbbfd8e900
Kizukkk <zentro934@gmail.com>
2023-03-05 13:37:55 +0200

    Додали файл DbContext для інціалізації БД

---
+++ Practice/Database/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Practice.Entity;
+
+namespace Practice.Database;
+
+public class ApplicationDbContext : DbContext
+{
+
+    public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options) :
+        base(options)
+    {
+        Database.EnsureCreated();
+    }
+
+    public DbSet<Dish> Dishes { get; set; }
+    public DbSet<Ingredient> Ingredients { get; set; }
+    public DbSet<User> Users { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+    }
+
+}
