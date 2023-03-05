86ec0be2c62662a53961581cc294ca56d3c2f67b
Kizukkk <zentro934@gmail.com>
2023-03-05 13:39:01 +0200

    Створили моделі для сутності User

---
+++ Practice/Models/UserCreateModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Practice.Models;
+
+public class UserCreateModel
+{
+    public string? Name { get; set; }
+
+    [NotNull]
+    [MinLength(6)]
+    public string? Email { get; set; }
+
+    [NotNull]
+    [MinLength(8)]
+    public string? Password { get; set; }
+
+}
---
+++ Practice/Models/UserViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Practice.Models;
+
+public class UserViewModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+
+
+}
