696e4bc4132901cb6fbab761b5c149c0c9101ab7
Kizukkk <zentro934@gmail.com>
2023-03-05 13:38:26 +0200

    Створили сутності з якими будемо працювати

---
+++ Practice/Entity/Dish.cs
@@ -0,0 +1,12 @@
+﻿namespace Practice.Entity;
+
+public class Dish
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public virtual ICollection<Ingredient>? Ingredients { get; set; }
+    public virtual ICollection<User>? Users { get; set; }
+
+
+}
---
+++ Practice/Entity/User.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace Practice.Entity;
+
+public class User
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public string? Password { get; set; }
+    public virtual ICollection<Dish>? Dishes { get; set;}
+}
