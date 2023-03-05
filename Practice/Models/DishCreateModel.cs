32e2842dd872851381ac692b20496258fc318bc9
Kizukkk <zentro934@gmail.com>
2023-03-05 13:44:08 +0200

    Додали модель для сутності Dishes

---
+++ Practice/Models/DishCreateModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Practice.Models;
+
+public class DishCreateModel
+{
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public ICollection<IngredientCreateModel>? Ingredients { get; set; }
+
+
+}
---
+++ Practice/Models/DishViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Practice.Models;
+
+public class DishViewModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public ICollection<IngredientViewModel> Ingredients { get; set; }
+
+}
