916c9731d54022d91b0c25f444c52b089281d310
Kizukkk <zentro934@gmail.com>
2023-03-06 13:13:43 +0200

    Моделі для ресурсу Ingredient

---
+++ Practice/Models/IngredientCreateModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Practice.Models;
+
+public class IngredientCreateModel
+{
+    [NotNull]
+    [MinLength(3)]
+    public string? Name { get; set; }
+
+    [NotNull]
+    [MinLength(6)]
+    public string? Description { get; set; }
+
+
+}
---
+++ Practice/Models/IngredientViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Practice.Models;
+
+public class IngredientViewModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
