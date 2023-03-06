5c6a67745f56a90a13746c2501dd4eabbe101717
Kizukkk <zentro934@gmail.com>
2023-03-06 13:12:36 +0200

    Додано Profile клас для трансферу даних між схожими моделями даних

---
+++ Practice/Extensions/AutoMapperProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Practice.Entity;
+using Practice.Models;
+
+namespace Practice.Extensions;
+
+public class AutoMapperProfile : Profile
+{
+    public AutoMapperProfile()
+    {
+        CreateMap<UserCreateModel, User>();
+        CreateMap<User, UserViewModel>();
+
+        CreateMap<IngredientCreateModel, Ingredient>();
+        CreateMap<Ingredient, IngredientViewModel>();
+
+        CreateMap<DishCreateModel, Dish>();
+        CreateMap<Dish, DishViewModel>();
+
+    }
+
+}
