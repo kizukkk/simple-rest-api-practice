539e36b85363e143d83078645cb838290208350c
Kizukkk <zentro934@gmail.com>
2023-03-06 13:13:25 +0200

    Додано залежності в механізм DI

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,15 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Practice.Database;
+using Practice.Extensions;
+using Practice.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
+
+builder.Services.AddTransient<IUserRepository, UserRepository>();
+builder.Services.AddTransient<IDishRepository, DishRepository>();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
